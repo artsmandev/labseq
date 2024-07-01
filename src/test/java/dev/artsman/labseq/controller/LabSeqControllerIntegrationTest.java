96a6b9847c94d007924660e480d30e8aa5f45301
Bruno Andrade <bruno@artsman.dev>
2024-07-01 09:10:41 +0100

    (refact) removed unnecessary import

--- src/test/java/dev/artsman/labseq/controller/LabSeqControllerIntegrationTest.java
+++ src/test/java/dev/artsman/labseq/controller/LabSeqControllerIntegrationTest.java
@@ -6,7 +6,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import dev.artsman.labseq.handler.LagSeqResponseEntityExceptionHandler;
 import dev.artsman.labseq.service.LabSeqService;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
